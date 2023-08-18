d5db5c345586c51837ab8224fd681c6e0e20776d
Santu7411 <86872669+Santu7411@users.noreply.github.com>
2023-08-18 18:05:46 +0800

    setup

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,6 +3,37 @@ import path from 'path';
 import Users from './collections/Users';
 import { payloadCloud } from '@payloadcms/plugin-cloud';
 import BeforeDashboard from './components/BeforeDashboard';
+import Categories from './collections/Categories';
+import Products from './collections/Products';
+import Skus from './collections/Skus';
+import Gifts from './collections/Gifts';
+import Targets from './collections/Targets';
+import Campaigns from './collections/Campaigns';
+import Assets from './collections/Assets';
+import TouchPoints from './collections/TouchPoints';
+import Engagements from './collections/Engagements';
+import GWPs from './collections/GWPs';
+import Achievements from './collections/Achievements';
+import Wallets from './collections/Wallets';
+import Feeds from './collections/Feeds';
+import Slides from './collections/Slides';
+import Pages from './collections/Pages';
+import Forms from './collections/Forms';
+import Notifications from './collections/Notifications';
+import Hints from './collections/Hints';
+import Resources from './collections/Resources';
+import References from './collections/References';
+import Coupons from './collections/Coupons';
+import Badges from './collections/Badges';
+import Tags from './collections/Tags';
+import Media from './collections/Media';
+import CalculatedEvents from './collections/CalculatedEvents';
+import InteractiveEvents from './collections/InteractiveEvents copy';
+import CartPromotions from './collections/CartPromotions';
+import RealtimeInteractives from './collections/RealtimeInteractives';
+import CustomEvents from './collections/CustomEvents';
+import CouponRules from './collections/CouponRules';
+import PointsRules from './collections/PointsRules';
 
 export default buildConfig({
   admin: {
@@ -13,9 +44,7 @@ export default buildConfig({
       beforeDashboard: [BeforeDashboard],
     }
   },
-  collections: [
-    Users,
-  ],
+  collections: [Categories, Campaigns, Feeds, TouchPoints, Products, Skus, Engagements, CartPromotions, RealtimeInteractives, CustomEvents, CouponRules, PointsRules, Assets, Targets, CalculatedEvents, InteractiveEvents, Wallets, Achievements, GWPs, Slides, Pages,Forms, Notifications, Hints, Resources,References, Tags, Users, Media, Gifts, Coupons, Badges],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
